8fee539f36748cbd319cd3d49f8f76dcbe0935f7
githubChenchi <chenchi_wy@163.com>
2017-07-22 17:05:42 +0800

    优化了所有的API

--- callback.c
+++ callback.c
@@ -5,14 +5,27 @@ callback_s callback[EVENT_MAX] = { { { NULL }, 0 } };
 
 void callback_register( event_t event, callback_t cb_fn )
 {
+	int count = 0;
+	
 	if(event >= EVENT_0 && event < EVENT_MAX)
 	{
-		if(callback[event].count < REGISTER_NUM_MAX)
+		for(count=0; count<REGISTER_NUM_MAX; count++)
 		{
-			callback[event].cb[callback[event].count++] = cb_fn;
-			printf("[SUCCESS] event [%d] has been registered [%d] times \r\n", event, callback[event].count);
+			if(callback[event].cb[count] == NULL)
+			{
+				if(cb_fn)
+				{
+					callback[event].cb[callback[event].count++] = cb_fn;
+					printf("[SUCCESS] event [%d] has been registered [%d] times \r\n", event, callback[event].count);
+				}
+				else
+				{
+					printf("[WARNING] event [%d] callback function can not be NULL \r\n", event);
+				}
+				break;
+			}
 		}
-		else
+		if(count == REGISTER_NUM_MAX)
 		{
 			printf("[FORBIDDEN] the callback functions register count of this event [%d] has been up to max \r\n", event);
 		}
@@ -25,22 +38,28 @@ void callback_register( event_t event, callback_t cb_fn )
 
 void callback_execute( event_t event )
 {
-	for(int count = 0; count < callback[event].count; count++)
+	int count = 0;
+
+	for(count=0; count<REGISTER_NUM_MAX; count++)
 	{
 		if(callback[event].cb[count])
 		{
 			callback[event].cb[count](NULL);
-		}		
-	}	
+		}	
+	}
 }
 
 void callback_unregister( event_t event, callback_t cb_fn )
 {
-	for(int count=0; count<callback[event].count; count++)
+	int count = 0;
+	for(count=0; count<REGISTER_NUM_MAX; count++)
 	{
 		if(callback[event].cb[count] == cb_fn)
 		{
 			callback[event].cb[count] = NULL;
+			callback[event].count--;
+			printf("[SUCCESS] event [%d] has been registered [%d] times \r\n", event, callback[event].count);
+			break;
 		}
 	}
 }
